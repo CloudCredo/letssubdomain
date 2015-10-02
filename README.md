b4faa4a6798ca52905e8841e4812f1f4a083919a
Gerhard Lazu <gerhard@lazu.co.uk>
2015-10-02 12:54:04 +0100

    Start with functional core for subdomain registration app

---
+++ README.md
@@ -0,0 +1,10 @@
+https://letssubdomain.platform.cloudcredo.io
+
+A web app to enable [Coud Foundry for Beginners: Zero to Hero](https://github.com/CloudCredo/training-cf-for-beginners)
+trainees to integrate a custom domain with their Cloud Foundry app.
+
+The app itself talks to the AWS Route53 API to register custom subdomains.
+Once registered, they will be used by the trainees e.g.
+`cf add-domain cf-z2h gerhard-cf-hero.platform.cloudcredo.io` for their own apps.
+
+All registered subdomains can be listed and deleted from the web app itself.
---
+++ spec/app_spec.rb
@@ -0,0 +1,48 @@
+require "app"
+
+RSpec.describe App do
+  subject(:app) { App.new(
+    dns_client: dns_client,
+  ) }
+  let(:dns_client) { double(:dns_client, resolve_dns_entry: dns_client_response) }
+  let(:success) { double(:success, call: nil) }
+  let(:failure) { double(:failure, call: nil) }
+
+  describe "#reserve_subdomain" do
+    before do
+      app.reserve_subdomain(
+        subdomain: "foo",
+        domain: "example.com",
+        ip: "1.1.1.1",
+        success: success,
+        failure: failure,
+      )
+
+      expect(dns_client).to have_received(:resolve_dns_entry).with(
+        subdomain: "foo",
+        domain: "example.com",
+        ip: "1.1.1.1",
+      )
+    end
+
+    context "when subdomain is available" do
+      let(:dns_client_response) { true }
+
+      it "succeeds with message" do
+        expect(success).to have_received(:call).with(
+          "foo.example.com now resolves to 1.1.1.1"
+        )
+      end
+    end
+
+    context "when subdomain is not available" do
+      let(:dns_client_response) { false }
+
+      it "fails with message" do
+        expect(failure).to have_received(:call).with(
+          "foo.example.com could not be reserved, try another subdomain"
+        )
+      end
+    end
+  end
+end
